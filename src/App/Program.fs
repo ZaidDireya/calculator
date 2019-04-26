034369a5471b076de8d25dc452cb2bd65d1b12b5
ZaidDireya <zaidde2005@gmail.com>
2019-04-27 00:52:38 +0300

    Calling Add with a negative number will throw an exception “negatives not allowed”

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -2,9 +2,24 @@
 
 open System
 open  System.Text.RegularExpressions
+exception NegativeValues of List<int>
+exception WrongPattren of String
 
+let rec Sum (x:List<String>) =
+      match x with
+      | [] -> 0, []
+      | a::tail ->
+          let a = System.Int32.Parse a
+          if a >= 0 then
+             let (sum , negative) = Sum tail
+             (sum + a), negative
+          else
+             let (sum, negative) = Sum tail
+             sum, a :: negative
+      
+      
 let ExtractDelimiter (x: String) =
-    let Matches = Regex.Match( x, "^\/\/(.+)\n(.+)")
+    let Matches = Regex.Match( x, "^\/\/\[(.+)\]\n(.+)")
     match Matches.Groups.Count with
       | 3 -> Matches.Groups.[1].Value , Matches.Groups.[2].Value
       | _ -> "," , x
@@ -12,12 +27,16 @@ let ExtractDelimiter (x: String) =
           
 let Add (x : String) =
   
-  let (delimiter, x) = ExtractDelimiter x
+  let (delimiter, numbers) = ExtractDelimiter x
   
-  let Pattern = String.concat delimiter ["^([0-9]+["; "])*[0-9]+"] 
-  match Regex.IsMatch( x, Pattern) with
-    | true -> Array.sumBy System.Int32.Parse (x.Split ( [|"," ; delimiter|], StringSplitOptions.RemoveEmptyEntries))
-    | false -> -1
+  let Pattern = String.concat delimiter ["^(-?[0-9]+"; ")*-?[0-9]+$"] 
+  match Regex.IsMatch( numbers, Pattern) with
+    | true ->
+        let (sum, negative) = Sum (Array.toList (numbers.Split ( [|"," ; delimiter|], StringSplitOptions.RemoveEmptyEntries)))
+        match negative with
+        | [] -> sum
+        | _ -> raise (NegativeValues negative)
+    | false -> raise (WrongPattren numbers)
 
 [<EntryPoint>]
 let main argv =
@@ -33,7 +52,8 @@ let main argv =
             try
               printfn "message is: %i" (Add msg)
             with
-              | invalidArg -> printfn "invalid arguments passed"
+              | NegativeValues(neglist) -> printfn "negatives not allowed %A" neglist
+              | WrongPattren(str) -> printfn "wrong pattren %s" msg
 
             // loop to top
             return! messageLoop()  
@@ -43,11 +63,11 @@ let main argv =
     )
     Actor.Post ""
     Actor.Post "1"
-    Actor.Post "1,2"
+    Actor.Post "1,-2"
     Actor.Post "//;\n2;14;44"
-    Actor.Post "//cc\n2;14;44"
-    Actor.Post "//cc\n2cc14cc44"
-    Actor.Post "//cc\n2;14;"
+    Actor.Post "//[cc]\n2;14;44"
+    Actor.Post "//[cc]\n2cc14cc44"
+    Actor.Post "//[cc]\n2;14;"
     Actor.Post "4,"
     Console.WriteLine("Press any key...")
     Console.ReadLine() |> ignore
